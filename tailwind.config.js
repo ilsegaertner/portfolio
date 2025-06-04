aeba36bdce40424713fd0a5b47d2197b05877dc3
Ilse Gaertner <joachimpruegl@gmail.com>
2025-06-04 09:24:16 +0200

    update styles, vite config, tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./about.html",
+    "./work.html",
+    "./contact.html",
+    "./weather-app.html",
+    "./meet-app.html",
+    "./legal-info.html",
+    "./cub-react.html",
+    "./cub-case-study.html",
+    "./cub-angular.html",
+    "./chat-app.html",
+    "./js/**/*.js",
+  ], // adjust paths!
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,25 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        about: path.resolve(__dirname, "about.html"),
+        work: path.resolve(__dirname, "work.html"),
+        contact: path.resolve(__dirname, "contact.html"),
+        weather: path.resolve(__dirname, "weather-app.html"),
+        meet: path.resolve(__dirname, "meet-app.html"),
+        legal: path.resolve(__dirname, "legal-info.html"),
+        cubReact: path.resolve(__dirname, "cub-react.html"),
+        cubCase: path.resolve(__dirname, "cub-case-study.html"),
+        cubAngular: path.resolve(__dirname, "cub-angular.html"),
+        chat: path.resolve(__dirname, "chat-app.html"),
+      },
+    },
+  },
 });
