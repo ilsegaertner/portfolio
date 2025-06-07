1f9f65848d2b4dca85d095a0c6a44ce9338e6534
Ilse Gaertner <joachimpruegl@gmail.com>
2025-06-07 11:38:24 +0200

    Updates

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import "../src/style.css";
+
 function openModal() {
   // const blurArea = document.getElementById("blur-item");
   const blurOverlay = document.getElementById("blur-overlay");
@@ -26,3 +28,22 @@ function closeModal() {
   modal.classList.add("translate-x-full", "opacity-0", "pointer-events-none");
   document.body.style.overflow = "";
 }
+
+const cards = document.querySelectorAll("project-list__item");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animate__animated", "animate__fadeInUp");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+cards.forEach((card, i) => {
+  observer.observe(card);
+  card.style.setProperty("--animate-delay", `${i * 100}ms`);
+});
